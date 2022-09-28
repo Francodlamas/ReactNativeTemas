46bc0daea660f9d5c77666fbd46540cb5217f1b5
Franco Damian Lamas <86671291+Francodlamas@users.noreply.github.com>
2022-09-28 09:15:57 -0300

    Add files via upload

---
+++ Screens/Home.js
@@ -0,0 +1,16 @@
+import React, { Component, useEffect, useState } from 'react';
+import { StyleSheet, Text, Image , View, TextInput, FlatList, StatusBar, SafeAreaView, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+
+const Home =({navigation})=>{
+
+
+
+
+
+return (
+    <Text>hdhsadsahdsahsa</Text>
+);}
+
+export default Home
